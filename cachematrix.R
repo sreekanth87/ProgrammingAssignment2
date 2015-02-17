d22bab6e48d248ace6cf1992b6d8fd8fd462a25f
sreekanth87 <sreekanth.mallikarjun@gmail.com>
2015-02-17 04:51:46 +0000

    Update cachematrix.R
    Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following two functions take any square invertible 
+#matrix and returns it's inverse.
 
-## Write a short comment describing this function
+#function 1: This is a parent function that stores the give matrix
+#caches the result.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<-function(m=matrix()){
+  i<-NULL
 
-}
+      get<-function() {
+          m           }
+          setinverse<-function(inverse){
+              i <<-inverse             }
+              getinverse<-function(){
+                    i               }
+list(get=get,setinverse=setinverse,getinverse=getinverse)
+                                    }
+#function2: This is the calling function that runs a conditional statement
+# to check if the cache for the given matrix is already computed. If yes,
+# it fetches the result from function1, else computes the result, stores it
+#in function1 and returns the result.
 
+cacheSolve<-function(m,...){
+  i<-m$getinverse()
+  
+  if(!is.null(i)){
+        message('getting cached result')
+        return(i)
+                 }
+  else{
+        message('calculating result...')
+        data<-m$get()
+        i<-solve(data)
+        m$setinverse(i)
+        return (i)  #does not print the inverse as it might be very large
+       }
+                            }
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#output:
+#> size<-1000
+#> source("matinv2.R")
+#> m<-matrix(sample(size^2),size,size)
+#> mm<-makeCacheMatrix(m)
+#> ss1<-cacheSolve(mm)
+###calculating result...
+#> ss2<-cacheSolve(mm)
+###getting cached result
+#> identical(ss1,ss2)
+###[1] TRUE
